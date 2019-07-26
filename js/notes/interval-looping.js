939fc3e3d4f9feed25688f520fa079bdad85501b
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-25 20:50:17 -0400

    update notes

--- js/notes/interval-looping.js
+++ js/notes/interval-looping.js
@@ -11,3 +11,10 @@ let myInterval = setInterval(() => {
 
 // Stop the loop
 clearInterval(myInterval)
+
+// Use interval to update dynamically. Decide which parts of code are updated when we make changes and which are updated when we run it once
+
+// Update function called from interval loop
+  // Scales/Axes need to change in case our data changes
+  // If scales/axes change, then we need to update size/position of rectangles
+  
