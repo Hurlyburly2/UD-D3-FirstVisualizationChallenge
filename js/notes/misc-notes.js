561e0e25ec0102fa0ac3a0f7182ea0d6b68d643f
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-01 00:21:00 -0400

    added layout and misc notes

---
+++ js/notes/misc-notes.js
@@ -0,0 +1,14 @@
+// Best resources for programming in D3 for boilerplate code:
+
+// D3 community is well-organized for sharing reusable code
+
+
+// Appropriate small, useful chunks from other peoples projects for your own:
+// bl.ocks.org
+// blockbuilder.org
+
+
+// Converting D3 version 4 to version 5
+
+//  Data loading uses promises (I already knew this)
+//  d3.schemeCategory20 color scheme removed (colors were too similar or something iunno)
